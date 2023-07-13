cebf09cf04b765b3682ec7266a36692d06ea22be
Justin <justin.martin@hsc.utah.edu>
2023-07-12 18:22:22 -0600

    start quiries

---
+++ db.js/db.js
@@ -0,0 +1,24 @@
+const db =('mysql2')
+
+function addDepartment(department) {
+
+}
+
+function addRole(role) {
+
+}
+
+function addEmployee(employee) {
+
+}
+
+function updateEmployeeRole(employeeId, newRoleId) {
+
+}
+
+module.exports = {
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
+};
