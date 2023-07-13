2c35d975a8e26bfd1eab31e03c183c87f911006b
Justin <justin.martin@hsc.utah.edu>
2023-07-12 18:30:11 -0600

    start seeds

---
+++ seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (id,name) VALUES 
+    (1,'Sales'),
+    (2,'Marketing')
+    (3,'Engineering');
+
+
+
+INSERT INTO role (id,title,salary,department_id) VALUES
+    ()
+    ()
+    ()
+    ()
+    ()
+    ()
+
+
+
+
+
+
+
+
+
+
+
+INSERT INTO employee
+
+
+
+
+
+
+
+
